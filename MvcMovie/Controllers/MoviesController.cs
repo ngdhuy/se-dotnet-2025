ea696db822a8c848a655631e46e747b6b085e43f
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2025-07-01 10:53:17 +0000

    feat: add basice view

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -6,20 +6,23 @@ namespace MvcMovie.Controllers;
 public class MoviesController : Controller
 {
     // GET: /movies/
-    public string Index()
+    public IActionResult Index()
     {
-        return "This is my default action...";
+        return View();
     }
 
     // GET: /movies/welcome
-    public string Welcome()
+    public IActionResult Welcome()
     {
-        return "This is the Welcome action method of Movies Controller";
+        ViewData["Message"] = "This is the Welcome action method of Movies Controller";
+        ViewData["NumTimes"] = 10;
+        return View();
     }
 
     // GET: /movies/get/:id?cat=value
-    public string Get(int id, string cat)
+    public IActionResult Get(int id, string cat)
     {
-        return "This is information of movies have id = " + id.ToString() + " with category is " + cat;
+        ViewData["Message"] = HtmlEncoder.Default.Encode($"This is information of movies have id = {id} with category is {cat}");
+        return View();
     }
 }
